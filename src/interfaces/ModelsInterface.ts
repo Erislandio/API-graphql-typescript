2018ac75b77a267e418c7471969fca5a9f954b4c
Erislândio Soares <erislandio.soares@acct.global>
2019-06-24 23:47:51 -0300

    Add model comments

--- src/interfaces/ModelsInterface.ts
+++ src/interfaces/ModelsInterface.ts
@@ -1,7 +1,9 @@
+import { CommentModel } from "./../models/CommentModel";
 import { PostModel } from "./../models/PostModel";
 import { UserModel } from "./../models/UserModel";
 
 export interface ModelsInteface {
   User: UserModel;
   Post: PostModel;
+  Comment: CommentModel;
 }
