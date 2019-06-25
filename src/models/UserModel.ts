762579eb30fc8bf4d8ac48373c81ba1d248f1a1c
Erislândio Soares <erislandio.soares@acct.global>
2019-06-24 23:08:53 -0300

    add user model

---
+++ src/models/UserModel.ts
@@ -0,0 +1,71 @@
+import * as Sequelize from "sequelize";
+import { BaseModelInterface } from "../interfaces/BaseModelInterface";
+
+export interface UserAtttributes {
+  id?: number;
+  name?: string;
+  email?: string;
+  password?: string;
+  photo?: string;
+}
+
+export interface UserInstance
+  extends Sequelize.Instance<UserAtttributes>,
+    UserAtttributes {
+  isPassword(encodedPassoword: string, password: string): boolean;
+}
+
+export interface UserModel
+  extends BaseModelInterface,
+    Sequelize.Model<UserInstance, UserAtttributes> {}
+
+export default (
+  sequelize: Sequelize.Sequelize,
+  DataTypes: Sequelize.DataTypes
+): UserModel => {
+  const User: UserModel = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: DataTypes.STRING(128),
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      photo: {
+        type: DataTypes.BLOB({
+          length: "long"
+        }),
+        allowNull: false,
+        defaultValue: null
+      }
+    },
+    {
+      tableName: "user",
+      hooks: {
+        // * criptografar a sennha do usuario antes de slavar a senha
+          beforeCreate: (user: UserInstance, options: Sequelize.CreateOptions): void => {
+
+          }
+      },
+    }
+  );
+
+  return User;
+};
