f6995f7990726edeac9dd38f15a36f69b2fcf901
Erislandio <erislandio.soares@acct.global>
2019-08-20 20:42:25 -0300

    add user modal

--- src/interfaces/ModelsInterface.ts
+++ src/interfaces/ModelsInterface.ts
@@ -1,3 +1,5 @@
+import { UserModel } from "../models/UserModel";
+
 export interface ModelsInterface {
     User: UserModel;
 }
--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,4 +1,5 @@
 import * as Sequelize from "sequelize";
+import { BaseModelInterface } from "../interfaces/BaseModelInterface";
 
 export interface UserAttributes {
   id?: number;
@@ -10,4 +11,51 @@ export interface UserAttributes {
 
 export interface UserInstance
   extends Sequelize.Instance<UserAttributes>,
-    UserAttributes {}
+    UserAttributes {
+  isPassword(encodedPassword: string, password: string): boolean;
+}
+
+export interface UserModel
+  extends BaseModelInterface,
+    Sequelize.Model<UserInstance, UserAttributes> {}
+
+export default (
+  sequelize: Sequelize.Sequelize,
+  DataTypes: Sequelize.DataTypes
+): UserModel => {
+  const user: UserModel = sequelize.define<UserInstance, UserAttributes>(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: DataTypes.STRING(128),
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      email: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        unique: true
+      },
+      photo: {
+        type: DataTypes.BLOB({
+          length: "long"
+        }),
+        allowNull: true
+      }
+    }
+  );
+
+  return user;
+};
