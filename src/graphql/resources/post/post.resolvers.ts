5f4d8ae7ba0181ee3dd49ee32c99c6b8e14962ec
Erislandio <erislandio.soares@acct.global>
2019-08-26 23:38:28 -0300

    add

--- src/graphql/resources/post/post.resolvers.ts
+++ src/graphql/resources/post/post.resolvers.ts
@@ -1,15 +1,33 @@
 import { DbConnection } from "../../../interfaces/DbConnectionInterface";
 import { GraphQLResolveInfo } from "graphql";
+import { PostInstance } from "../../../models/PostModel";
 
 export const postResolvers = {
     Query: {
-        posts: (parent, {first, offset}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+
+        Post: {
+
+            author: (parent: PostInstance, {first = 10, offset = 0}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+                return db.User.findById(parent.get('author'))
+                
+            },
+        },
+        posts: (parent, {first = 10, offset = 0}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
 
             return db.Post.findAll({
                 limit: first,
                 offset
             })
 
+        },
+        post: (parent, {id}, {db}: {db: DbConnection}, info: GraphQLResolveInfo) => {
+            return db.Post.findById(id).then((post: PostInstance) => {
+                if(!post) {
+                    throw new Error(`Post with id ${id} not found`)
+                }
+
+                return post
+            })
         }
-    }
+    },
 }
