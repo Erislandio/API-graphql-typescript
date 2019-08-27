1a2d90c4b2be689756c2a5f2169c4c788320cdf3
Erislandio <erislandio.soares@acct.global>
2019-08-26 21:32:02 -0300

    add user schema

---
+++ src/graphql/resources/user/user.schema.ts
@@ -0,0 +1,43 @@
+const userTypes = `
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        photo: String!
+        createdAt: String!
+        updatedAt: String!
+    }
+
+    input UserInput {
+        name: String!
+        email: String!
+        password: String!
+    }
+
+    input UserUpdateInput {
+        name: String!
+        email: String!
+        photo: String!
+    }
+
+    input UserUpdatePass {
+        password: String!
+    }
+
+`
+
+
+const userQueries = `
+    users(first: Int, offset: Int): [User!]!
+    user(id: ID!): User
+`
+
+const userMutations = `
+
+    createUser(input: UserInput!): User
+    updateUser(id: ID!, input: UserUpdateInput!): User
+    updateUserPass(id: ID!, input: UserUpdatePass!): Boolean
+    deleteUser(id: ID!): Boolean
+
+`
