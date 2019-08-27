c042ae7fbfd305a0197963296120eee86784bd44
Erislandio <erislandio.soares@acct.global>
2019-08-26 22:56:23 -0300

    add user query resolver

--- dist/graphql/schema.js
+++ dist/graphql/schema.js
@@ -4,6 +4,8 @@ const graphql_tools_1 = require("graphql-tools");
 const query_1 = require("./query");
 const mutation_1 = require("./mutation");
 const user_schema_1 = require("./resources/user/user.schema");
+const post_schema_1 = require("./resources/post/post.schema");
+const comment_schema_1 = require("./resources/comments/comment.schema");
 const SchemaDefinition = `
 
   type Schema {
@@ -13,5 +15,5 @@ const SchemaDefinition = `
 
 `;
 exports.default = graphql_tools_1.makeExecutableSchema({
-    typeDefs: [SchemaDefinition, query_1.Query, mutation_1.Mutation, user_schema_1.userTypes]
+    typeDefs: [SchemaDefinition, query_1.Query, mutation_1.Mutation, user_schema_1.userTypes, post_schema_1.PostTypes, comment_schema_1.commentTypes]
 });
